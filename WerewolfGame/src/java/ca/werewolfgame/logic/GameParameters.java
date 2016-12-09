bc51234098510930f4d7424941d7930860edde2e
Joao Pedro <jpedrosffair@gmail.com>
2016-12-09 16:54:10 -0500

    Add files via upload

---
+++ WerewolfGame/src/java/ca/werewolfgame/logic/GameParameters.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ca.werewolfgame.logic;
+
+/**
+ *
+ * @author jpedr
+ */
+public class GameParameters {
+    
+    //time in seconds (300 = 5 minutes)
+    public static long getRoundDuration = 300;
+    
+    public static int mainChatHistorySize = 10;
+    public static int gameChatHistorySize = 10;
+    public static int wwChatHistorySize = 10;
+    public static int deadChatHistorySize = 10;
+    
+
+    
+}
